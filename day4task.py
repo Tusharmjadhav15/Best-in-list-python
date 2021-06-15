b2d2fa0e9470c95d96fa05cbf37ae808724a4126
TUSHAR MAHENDRA JADHAV <85783873+Tusharmjadhav15@users.noreply.github.com>
2021-06-15 10:11:09 +0530

    day 4 assignment

---
+++ day4task.py
@@ -0,0 +1,28 @@
+# create three variables with same value
+
+a = b = c = 20
+
+# divide a by 10
+divi = a/10
+print(int(divi))
+
+# multiply b by 50
+mul = b * 50
+print(mul)
+
+# add c value by 60
+add_1 = c + 60
+print(add_1)
+
+# create string of 5 charcters and replace 3rd character with G
+var_1 = "asdfg"
+x = var_1.replace(var_1[2],"G")
+print(x)
+
+# create two values of int float datatypes and covert them viceversa
+a,b = 22,35.4
+a1 = (float(a))
+b1 = (int(b))
+print(a1,b1)
+
+
